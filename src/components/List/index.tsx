d620b21fb903466d98b6602d80732f25106441cb
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-11-05 20:08:34 -0300

    refactor: improve List component with dynamic rendering based on type props
    add 'type' prop to determine whether to render a users list or repos list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,15 +2,30 @@ import React, { useContext } from 'react';
 import User from '../User';
 import { SearchContext } from '../../contexts/searchContext';
 import * as S from './style';
+import { UsersContext } from '../../contexts/usersContext';
+import Repo from '../Repo';
 
-export default function List(): React.JSX.Element {
+interface Props {
+  type: 'users' | 'repos';
+}
+
+export default function List({ type }: Props): React.JSX.Element {
   const { result } = useContext(SearchContext);
+  const { repos } = useContext(UsersContext);
 
+  const renderContent = (): React.JSX.Element[] | null => {
+    if (type === 'users' && result != null) {
+      return result.map((element) => (
+        <User search={element} key={element.id} />
+      ));
+    } else if (type === 'repos' && repos !== null) {
+      return repos.map((element) => <Repo data={element} key={element.id} />);
+    }
+    return null;
+  };
   return (
     <S.Wrapper>
-      <S.List>
-        {result?.map((element) => <User search={element} key={element.id} />)}
-      </S.List>
+      <S.List>{renderContent()}</S.List>
     </S.Wrapper>
   );
 }
